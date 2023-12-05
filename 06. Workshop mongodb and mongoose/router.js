1a1a5a1b046935b792c4f610c7ff3c81affdd84c
Mustafa Dzhindzhi <122883380+mustafadzhindzhi@users.noreply.github.com>
2023-12-05 10:22:11 +0200

    Add files via upload

---
+++ 06. Workshop mongodb and mongoose/router.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const homeController = require('./controllers/homeController.js');
+const cubeController = require('./controllers/cubController.js');
+
+router.use(homeController);
+router.use('/cubes', cubeController);
+router.get('*', (req, res) => {
+    res.redirect('/404');
+})
+
+module.exports = router;
---
+++ 06. Workshop mongodb and mongoose/services/cubeServices.js
@@ -0,0 +1,30 @@
+const uniqid = require('uniqid'); 
+const cubes = [];
+
+exports.create = (cubeData) => { 
+
+    const newCube = {
+        id: uniqid(),
+        ...cubeData
+    };
+    cubes.push(newCube);
+    return newCube;
+};
+
+exports.getAll = (search, from, to) => {
+    let filterCubes = [...cubes];
+    if (search) {
+        filterCubes = filterCubes.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
+    if (from) {
+        filterCubes = filterCubes.filter((cube) => cube.difficultyLevel >= Number(from));
+    }
+    if (to) {
+        filterCubes = filterCubes.filter((cube) => cube.difficultyLevel <= Number(from));
+    };
+    return filterCubes;
+};
+
+exports.getSingleCube = (id) => {
+    return cubes.find(cube => cube.id === id);
+}
